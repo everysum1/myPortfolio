c61a28c65e1095ceff90556ad0b0ae33553407e0
Izzy <izzy@Izzys-MacBook-Pro.local>
2017-01-23 22:05:52 -0800

    Add basic Projects component

---
+++ src/Projects.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+
+class Projects extends Component {
+  render() {
+    return (
+      <div className="Projects">
+        <h1>Projects</h1>
+        <img src="https://placehold.it/300x200" alt="" />
+        <img src="https://placehold.it/300x200" alt="" />
+      </div>
+    )
+  }
+}
+
+export default Projects;

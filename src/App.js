1e5e1715229a1985b8516a71d913afafa2042ea8
Izzy <izzy@Izzys-MacBook-Pro.local>
2017-01-23 21:45:53 -0800

    Add sections to landing page layout

--- src/App.js
+++ src/App.js
@@ -1,11 +1,24 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import Header from './Header';
+import Jumbotron from './Jumbotron';
+import About from './About';
+import Projects from './Projects';
+import Experience from './Experience';
+import Contact from './Contact';
+
 
 class App extends Component {
   render() {
     return (
       <div className="App">
+        <Header />
+        <Jumbotron />
+        <About />
+        <Projects />
+        <Experience />
+        <Contact />
         <div className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
           <h2>Welcome to Israel's Portfolio Page</h2>

632519afc55ea7ef2ac0b5881b9d3c502daae849
Izzy <izzy@Izzys-MacBook-Pro.local>
2017-01-23 22:27:19 -0800

    Add filler text to About component

--- src/About.js
+++ src/About.js
@@ -5,7 +5,21 @@ class About extends Component {
     return (
       <div className="About">
         <h1>About</h1>
-        <p>This is my about section</p>
+        <p>
+          Whatever fap man bun banh mi salvia tote bag, health goth hella 
+          farm-to-table. Distillery semiotics chicharrones, gastropub iPhone 
+          selvage bitters squid 8-bit +1 migas vegan vaporware you probably haven't 
+          heard of them. Af twee ugh, schlitz disrupt fap hexagon activated charcoal 
+          kickstarter selvage try-hard small batch brunch. Chartreuse squid helvetica 
+          flexitarian 3 wolf moon. 
+        </p>
+
+        <p>
+          Thundercats wolf intelligentsia, tote bag stumptown 
+          single-origin coffee truffaut fingerstache mumblecore snackwave sartorial 
+          shabby chic flexitarian master cleanse messenger bag. Slow-carb knausgaard 
+          kogi vape. Bitters YOLO drinking vinegar art party tattooed, pickled deep v.
+        </p>
       </div>
     )
   }
